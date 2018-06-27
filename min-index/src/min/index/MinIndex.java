668cdf4757d5c6e1538ac7c1c6821e0bee000d97
gc_science10 <gc_science10@SE135-05.ad.svsu.edu>
2018-06-27 10:39:50 -0400

    unfinished code. user can now set an array length and give values to the array. the sorting still has problems

--- min-index/src/min/index/MinIndex.java
+++ min-index/src/min/index/MinIndex.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package min.index;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +15,32 @@ public class MinIndex {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args){
+        System.out.println("pls input array length");
+        Scanner scanf = new Scanner(System.in);
+        int arrlength = scanf.nextInt();
+        int[] arr = new int[arrlength];
+        
+        for(int x=0 ; x<arrlength ; x++){
+            System.out.println("place value for number " + x);
+            int value = scanf.nextInt();
+            arr[x] = value;
+            
+        }
+        int smallnum = 0;
+        for(int x=0 ; x<arrlength-1 ; x++){
+            
+            int y = x+1; 
+            if(arr[x]<arr[y]){
+            smallnum = arr[x];
+            }
+            
+            
+        }
+        
+        
+        System.out.println("smallest number is" + smallnum);
+        
     }
     
 }
