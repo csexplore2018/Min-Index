2106624a665c1d1cf0f7c4530c57a088aef20d4c
gc_science10 <gc_science10@SE135-05.ad.svsu.edu>
2018-06-27 09:53:08 -0400

    empty program for min index

---
+++ min-index/src/min/index/MinIndex.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package min.index;
+
+/**
+ *
+ * @author gc_science10
+ */
+public class MinIndex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
